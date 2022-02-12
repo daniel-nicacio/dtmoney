075bf81a906853b87056a69fa4d467c8f2c9154f
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-02-12 16:29:42 -0300

    initial commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Container } from "./styles";
+import { api } from "../../services/api";
+
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  category: string;
+  amount: number;
+  createdAt: string;
+}
+
+export const TransactionsTable = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get("/transactions").then((response) => {
+      setTransactions(response.data.transactions);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-br").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
+};
